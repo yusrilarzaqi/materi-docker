c901a595ebc56178c4bfdff983c6d745034f7dea
yusrilarzaqi <arzaqiyusril@gmail.com>
2022-08-08 18:50:37 +0700

    add materi Command Instruction

--- docker-file/script.sh
+++ docker-file/script.sh
@@ -1,4 +1,18 @@
+# build 
 # docker build -t yusrilarzaqi/from from
 
-# docker build -t yusrilarzaqi/run run 
-docker build -t yusrilarzaqi/run run  --progress=plain --no-cache
+# build and run
+# docker build -t yusrilarzaqi/run run
+
+# build and run with display
+# docker build -t yusrilarzaqi/run run  --progress=plain --no-cache
+
+# build , run and command
+docker build -t yusrilarzaqi/command command
+
+# create new container
+docker container create --name command yusrilarzaqi/command
+# start container command
+docker container start command
+# log dari container command
+docker container logs command
